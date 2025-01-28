fb048f1e5c838183c7ed072d925eb9e73528e5aa
UmiduKaushika97 <umidu.kaushika@gmail.com>
2025-01-28 13:36:15 +0530

    Header component create

---
+++ src/components/ThemeToggle/ThemeToggle.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from 'react'
+import { FaSun, FaMoon } from 'react-icons/fa';
+import '../ThemeToggle/ThemeToggle.css'
+
+const ThemeToggle = () => {
+    const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');
+  };
+  return (
+    <div className="theme-toggle" onClick={toggleTheme}>
+    <div className={`toggle-slider ${theme === 'dark' ? 'dark' : 'light'}`}>
+      {theme === 'light' ? (
+        <FaSun className="toggle-icon" />
+      ) : (
+        <FaMoon className="toggle-icon" />
+      )}
+    </div>
+  </div>
+  )
+}
+
+export default ThemeToggle
