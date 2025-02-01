f78f9d6e8156c273b795583c91716fae51e8a3d4
UmiduKaushika97 <umidu.kaushika@gmail.com>
2025-02-01 15:37:44 +0530

    All Projects add

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,5 +1,12 @@
 import Work1 from '../../assets/Work1.png';
 import Work3 from '../../assets/Work3.png';
+import Work4 from '../../assets/Work4.png';
+import Work5 from '../../assets/Work5.png';
+import Work6 from '../../assets/Work6.png';
+import Work7 from '../../assets/Work7.png';
+import Work8 from '../../assets/Work8.png';
+import Work9 from '../../assets/Work9.png';
+import Work10 from '../../assets/Work10.png';
 
 export const projectsData = [
     {
@@ -21,6 +28,78 @@ export const projectsData = [
         sourceUrl: '',
         status: 'completed',
     },
+
+    {
+        id: 3,
+        image: Work4,
+        title: 'Todo List',
+        category: 'web',
+        demoUrl: 'https://todo-list-umidu.netlify.app',
+        sourceUrl: '',
+        status: 'completed',
+    },
+
+    {
+        id: 4,
+        image: Work5,
+        title: 'Shopping Cart Web',
+        category: 'web',
+        demoUrl: 'https://shopping-bag-umidu.netlify.app',
+        sourceUrl: '',
+        status: 'completed',
+    },
+
+    {
+        id: 5,
+        image: Work6,
+        title: 'Quiz Game Web',
+        category: 'web',
+        demoUrl: 'https://quiz-game-umidu.netlify.app',
+        sourceUrl: '',
+        status: 'completed',
+    },
+
+    {
+        id: 6,
+        image: Work7,
+        title: 'Task Manager',
+        category: 'web',
+        demoUrl: 'https://task-manager-umidu.netlify.app',
+        sourceUrl: '',
+        status: 'completed',
+    },
+
+    {
+        id: 7,
+        image: Work8,
+        title: 'Self Service Order App',
+        category: 'app',
+        demoUrl: 'https://self-service-order-umidu.netlify.app',
+        sourceUrl: '',
+        status: 'completed',
+    },
+
+    {
+        id: 8,
+        image: Work9,
+        title: 'Band Member App',
+        category: 'app',
+        demoUrl: 'https://band-members-app-umidu.netlify.app',
+        sourceUrl: '',
+        status: 'completed',
+    },
+
+    {
+        id: 9,
+        image: Work10,
+        title: 'Todo List App',
+        category: 'app',
+        demoUrl: 'https://todo-list-app-umidu.netlify.app',
+        sourceUrl: '',
+        status: 'completed',
+    },
+
+    
 ]
 
 export const projectsNav = [
@@ -29,9 +108,9 @@ export const projectsNav = [
         name : 'all',
     },
 
-    // {
-    //     name: 'app',
-    // },
+    {
+        name: 'app',
+    },
 
     {
         name: 'web',
